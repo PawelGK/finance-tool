9f87163fda391be341172122e9be88ab856b7a92
rusleak <hotlinetony2@gmail.com>
2026-05-05 12:53:19 +0200

    feat: remaining income method

--- finance.py
+++ finance.py
@@ -2,4 +2,8 @@ def savings_goal(monthly_income, percentage):
     return monthly_income * (percentage / 100)
 
 if __name__ == "__main__":
-    print("Savings goal:", savings_goal(5000, 20))
+    print("Savings target amount:", savings_goal(5000, 20))
+
+def remaining_income(income, expenses):
+    return income - expenses
+print("Remaining income:", remaining_income(5000, 3200))
